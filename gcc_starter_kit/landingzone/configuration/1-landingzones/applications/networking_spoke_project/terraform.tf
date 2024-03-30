2f638ae4801bc72f252e3a03f58086f03f42af92
thiamsoontan <thiamsoontan@microsoft.com>
2024-03-30 17:30:57 +0800

    cleanup

--- gcc_starter_kit/landingzone/configuration/1-landingzones/applications/networking_spoke_project/terraform.tf
+++ gcc_starter_kit/landingzone/configuration/1-landingzones/applications/networking_spoke_project/terraform.tf
@@ -1,9 +1,5 @@
 provider "azurerm" {
   features {}
-
-  subscription_id = data.azurerm_client_config.current.subscription_id
-  tenant_id       = data.azurerm_client_config.current.tenant_id
-  use_msi        = true
 }
 
 # Configure Terraform backend
